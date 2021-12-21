c52f22658d9ebdcbba4ddc483cd7f253fb837026
sagar kunwar <sagarkunwar610@gmail.com>
2021-12-21 16:48:18 +0545

    hello react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
+
+const Book = () => {
+  return (
+    <section>
+      <Image />
+      <Title />
+      <Author />
+    </section>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/719S4nXwvxL._AC_UY327_FMwebp_QL65_.jpg"
+      alt="The Journey Home: Autobiography of an American Swami"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>The Journey Home: Autobiography of an American Swami</h1>;
+};
+
+const Author = () => {
+  return <h4>By : Radhanath Swami </h4>;
+};
 
 export default App;
